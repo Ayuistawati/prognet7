adef21e7cfec1dac0ad8cf96bb6aa2571713f94b
ayuistawati <istawatiayu51@gmail.com>
2023-11-09 10:50:34 +0800

    push tugas

---
+++ app/Http/Controllers/TugasLaravelController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TugasLaravel;
+use Illuminate\Http\Request;
+
+class TugasLaravelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TugasLaravel $tugasLaravel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TugasLaravel $tugasLaravel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TugasLaravel $tugasLaravel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TugasLaravel $tugasLaravel)
+    {
+        //
+    }
+}
